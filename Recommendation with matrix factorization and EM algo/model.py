7acf1ad4be0d4905ec95229539a5713c4429483f
yesbo <hz2501@columbia.edu>
2018-11-14 13:30:22 +0800

    d
    s

--- Recommendation with matrix factorization and EM algo/model.py
+++ Recommendation with matrix factorization and EM algo/model.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-%matplotlib inline
 import os
 from scipy.stats import norm
 from scipy.stats import multivariate_normal as m_norm
